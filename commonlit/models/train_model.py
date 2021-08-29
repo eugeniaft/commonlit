290441a280bf72d3af90e69257510be0294ede82
Eugenia Fernandez <eugeniaft@users.noreply.github.com>
2021-08-29 16:31:57 -0400

    added bow function

--- commonlit/models/train_model.py
+++ commonlit/models/train_model.py
@@ -1,1 +1,33 @@
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
+
+def get_bow(corpus, bow_type, ngram_range=(1, 1), tokenizer=None):
+
+    '''
+    Converts corpus into matrix of token counts using sklearn
+    implementation. bow_type options include: tf-idf,
+    binarized (binarized counts of tokens), or count.
+    Returns matrix and vectorizer instance
+    '''
+
+    if bow_type == 'tf-idf':
+        vectorizer = TfidfVectorizer(ngram_range=ngram_range,
+                                     tokenizer=tokenizer)
+    if bow_type == 'binarized':
+        vectorizer = CountVectorizer(ngram_range=ngram_range,
+                                     tokenizer=tokenizer,
+                                     binary=True)
+    if bow_type == 'count':
+        vectorizer = CountVectorizer(ngram_range=ngram_range,
+                                     tokenizer=tokenizer)
+
+    X = vectorizer.fit_transform(corpus)
+
+    return X, vectorizer
+
+
+if __name__ == "__main__":
+
+    corpus = ['test 1', 'test 2']
+    X, vectorizer = get_bow(corpus, 'count')
+    print(X.toarray())
